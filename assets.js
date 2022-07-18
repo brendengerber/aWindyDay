9cc8e1a0236913a281e4c656150fb2690efb3cc1
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2022-07-18 13:44:40 -0500

    Add fence asset

--- assets.js
+++ assets.js
@@ -92,6 +92,7 @@ module.exports.Tree = class {
 
 
 module.exports.Horizon = class{
+    //Take an integer as an argument for how long to draw the horizon.
     constructor(horizonLength){
         this.frame1 = this.buildFrame(horizonLength)
     }
@@ -104,6 +105,48 @@ module.exports.Horizon = class{
         return frame
     }
 }
+module.exports.Fence = class {
+    //Takes integers for the size of the field it will contain.
+    constructor(x,y){
+        this.frame1 = this.buildFrame(x,y)
+    }
+    buildFrame(x,y){
+        let frame = []
+        //Builds the left side of the frame.
+        for(let i = y; i > 0; i--){
+            frame.push(['‡'])
+        }
+        //Fills the middle with 'blank' to make it transparent.
+        for(let i = x; i > 0; i--){
+            for(let row of frame){
+                row.push('blank')
+            }
+        }
+        //Builds the right side of the fence.
+        for(let row of frame){
+            row.push('‡')
+        }
+        //Builds the bottom of the fence.
+        frame.push([])
+        for(let i = x + 2; i > 0; i--){
+            frame[frame.length - 1].push('‡')
+        }
+        //Builds the top of the fence.
+        frame.unshift([])
+        for(let i = x + 2; i > 0; i--){
+            frame[0].push('‡')
+        }
+        //Builds the fence's door.
+        if(frame[0].length % 2 === 0){
+            frame[0][frame[0].length/2 - 1] = '-'
+            frame[0][frame[0].length/2] = '-'
+        }else{
+            frame[0][Math.floor((frame[0].length/2))] = '-'
+        }
+        return frame
+    }
+}
+
 
 
 
